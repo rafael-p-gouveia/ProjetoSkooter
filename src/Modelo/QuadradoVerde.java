70d81261a15347f2df55c3c7ac9cfa9f047107ca
R <R@R-PC>
2021-06-15 17:15:21 -0300

    Inicializando projeto

--- src/Modelo/QuadradoVerde.java
+++ src/Modelo/QuadradoVerde.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package Modelo;
+import Auxiliar.Desenhador;
 import Auxiliar.Posicao;
 
 /**
@@ -16,6 +17,38 @@ public class QuadradoVerde extends Elemento {
         super("bloco_losango_verde.jpg");
         this.setPosicao(umaPosicao);
         bTransponivel = false;
+    }
+    
+    @Override
+    public void interage(Hero h){
+        
+        switch(h.iIndexOfImage){
+            case 0:
+                if(Desenhador.getTelaDoJogo().ehPosicaoValida(new Posicao(getPosicao().getLinha() + 1, getPosicao().getColuna()))){
+                    this.moveDown();
+                    h.setPosicao(pPosicao);
+                }
+                break;
+            case 1:
+                if(Desenhador.getTelaDoJogo().ehPosicaoValida(new Posicao(getPosicao().getLinha(), getPosicao().getColuna() - 1))){
+                    this.moveLeft();
+                    h.setPosicao(pPosicao);
+                }
+                break;
+            case 2:
+                if(Desenhador.getTelaDoJogo().ehPosicaoValida(new Posicao(getPosicao().getLinha() - 1, getPosicao().getColuna()))){
+                    this.moveUp();
+                    h.setPosicao(pPosicao);
+                }
+                break;
+            case 3:
+                if(Desenhador.getTelaDoJogo().ehPosicaoValida(new Posicao(getPosicao().getLinha(), getPosicao().getColuna() + 1))){
+                    this.moveRight();
+                    h.setPosicao(pPosicao);
+                }
+                break;
+        }
+        
     }
     
 }
